40e9bbf19d6b0265e63a048c668b4ddb86762afb
AlissonSauer1538 <160769393+AlissonSauer1538@users.noreply.github.com>
2024-02-25 21:51:03 -0300

    AT1.java

---
+++ AT1.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String nome;
+        float valor = 5.2f;
+        int idade = 15;
+        Integer ano = 90;
+        double peso = 40.5;
+        boolean confirma = true;
+        char sexo = 'M';
+        long numG = 323432423434;
+        byte numP = 6834
+        short numC = 123
+    }
+}
