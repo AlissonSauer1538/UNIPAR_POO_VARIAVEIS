79fce9c269925483ded194fcd63348d9bfd96409
AlissonSauer1538 <160769393+AlissonSauer1538@users.noreply.github.com>
2024-02-25 21:55:15 -0300

    AT4.java

---
+++ AT4.java
@@ -0,0 +1,10 @@
+public class ControleEstoque {
+    public static void main(String[] args) {
+        // Declaração das variáveis aqui
+        String nomeproduto;
+        String categoriaproduto
+        double precounitario;
+        int qtdemproduto
+
+    }
+}
